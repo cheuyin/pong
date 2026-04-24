4de9bbc3830e88e13c806492162e4816e1156c5b
cheuyin <yinstanleycheung@gmail.com>
2026-04-24 09:10:54 -0700

    Create boilerplate game loop

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import pygame
 
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    # Process player inputs
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Logical updates here
+    # ...
+
+    screen.fill("black")
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()

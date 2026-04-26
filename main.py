88f6125872aed7729835a0e4a82a1bafe20f136c
cheuyin <yinstanleycheung@gmail.com>
2026-04-26 11:07:37 -0700

    Check which player won the round and print respective scores

--- main.py
+++ main.py
@@ -57,12 +57,22 @@ class Game:
     def _check_ball_out_of_bounds(self):
         screen_rect = self.screen.get_rect()
         ball_rect = self.ball.rect
-        if ball_rect.right <= 0 or ball_rect.left >= screen_rect.width:
-            self._round_over()
+        if ball_rect.right <= 0:
+            self._round_over("player 2")
+        elif ball_rect.left >= screen_rect.width:
+            self._round_over("player 1")
 
-    def _round_over(self):
+    def _round_over(self, player_that_won):
         sleep(0.5)
+        if player_that_won == "player 1":
+            self.player1_score += 1
+        elif player_that_won == "player 2":
+            self.player2_score += 1
         self.ball.reset()
+
+        print("===== SCORE =====")
+        print(f"Player 1: {self.player1_score}")
+        print(f"Player 2: {self.player2_score}")
 
 
 if __name__ == "__main__":
